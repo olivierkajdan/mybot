51159dc9ad1ae8077fd3d03f5b6189eb659a1afb
Olivier Kajdan <olivierk@microsoft.com>
2018-01-21 22:36:26 +0100

    commit

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ server.listen(process.env.PORT || 3000, function()
 
 // Create chat bot
 var connector = new builder.ChatConnector
-({ appId: 'YourAppId', appPassword: 'YourAppPassword' }); 
+({ appId: 'OlivBot', appPassword: 'gZIDQ91_[ohhovgIPN779:(' }); 
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
